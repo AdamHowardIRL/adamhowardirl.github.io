44ce11f72b655085824af69c687f41c9dbf3c889
Adam Howard <adam.howard@mycit.ie>
2017-10-21 22:09:24 +0100

    34242342

---
+++ src/components/Sidebar/_nav.js
@@ -0,0 +1,220 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+    {
+      title: true,
+      name: 'Screens',
+      wrapper: {            // optional wrapper object
+        element: "span",      // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ""             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Screen 1',
+      url: '/components',
+      icon: '',
+      children: [
+        {
+          name: 'Advert 1',
+          url: '/Screen 1/Advert 1',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 2',
+          url: '/Screen 1/Advert 2',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 3',
+          url: '/Screen 1/Advert 3',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 4',
+          url: '/Screen 1/Advert 4',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 5',
+          url: '/Screen 1/Advert 5',
+          icon: 'icon-puzzle'
+        }
+      ]
+    },
+
+    {
+      name: 'Screen 2',
+      url: '/components',
+      icon: 'desktop',
+      children: [
+        {
+          name: 'Advert 1',
+          url: '/Screen 1/Advert 1',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 2',
+          url: '/Screen 1/Advert 2',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 3',
+          url: '/Screen 1/Advert 3',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 4',
+          url: '/Screen 1/Advert 4',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 5',
+          url: '/Screen 1/Advert 5',
+          icon: 'icon-puzzle'
+        }
+      ]
+    },
+
+    {
+      name: 'Screen 3',
+      url: '/components',
+      icon: 'desktop',
+      children: [
+        {
+          name: 'Advert 1',
+          url: '/Screen 1/Advert 1',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 2',
+          url: '/Screen 1/Advert 2',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 3',
+          url: '/Screen 1/Advert 3',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 4',
+          url: '/Screen 1/Advert 4',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 5',
+          url: '/Screen 1/Advert 5',
+          icon: 'icon-puzzle'
+        }
+      ]
+    },
+
+    {
+      name: 'Screen 4',
+      url: '/components',
+      icon: 'desktop',
+      children: [
+        {
+          name: 'Advert 1',
+          url: '/Screen 1/Advert 1',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 2',
+          url: '/Screen 1/Advert 2',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 3',
+          url: '/Screen 1/Advert 3',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 4',
+          url: '/Screen 1/Advert 4',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 5',
+          url: '/Screen 1/Advert 5',
+          icon: 'icon-puzzle'
+        }
+      ]
+    },
+
+    {
+      name: 'Screen 5',
+      url: '/components',
+      icon: 'desktop',
+      children: [
+        {
+          name: 'Advert 1',
+          url: '/Screen 1/Advert 1',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 2',
+          url: '/Screen 1/Advert 2',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 3',
+          url: '/Screen 1/Advert 3',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 4',
+          url: '/Screen 1/Advert 4',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 5',
+          url: '/Screen 1/Advert 5',
+          icon: 'icon-puzzle'
+        }
+      ]
+    },
+
+    {
+      name: 'Screen 6',
+      url: '/components',
+      icon: 'desktop',
+      children: [
+        {
+          name: 'Advert 1',
+          url: '/Screen 1/Advert 1',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 2',
+          url: '/Screen 1/Advert 2',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 3',
+          url: '/Screen 1/Advert 3',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 4',
+          url: '/Screen 1/Advert 4',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Advert 5',
+          url: '/Screen 1/Advert 5',
+          icon: 'icon-puzzle'
+        }
+      ]
+    }
+   
+  ]
+};
